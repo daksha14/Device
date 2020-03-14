82943bae8f16037876674ee1db3b212bd36c2119
daksha14 <daksha.logicalstreet@gmail.com>
2020-03-14 12:46:06 +0530

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:image,:firstname, :lastname,:email, :password, :password_confirmation, :current_password) }
+  end
+end
